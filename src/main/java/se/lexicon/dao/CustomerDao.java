4190488add2b57dd97d526cf7432844d92aa08f1
liljana ristevska <li.ristevska@gmail.com>
2023-01-11 14:24:42 +0100

    added

--- src/main/java/se/lexicon/dao/CustomerDao.java
+++ src/main/java/se/lexicon/dao/CustomerDao.java
@@ -1,6 +1,11 @@
 package se.lexicon.dao;
 
+import se.lexicon.exception.DataNotFoundException;
 import se.lexicon.model.Customer;
 
+import java.util.List;
+
 public interface CustomerDao extends BaseDao<Customer, Long> {
+
+    List<Customer> finBuName(String firstName) ;
 }
--- src/main/java/se/lexicon/dao/impl/CustomerDaoImpl.java
+++ src/main/java/se/lexicon/dao/impl/CustomerDaoImpl.java
@@ -3,12 +3,14 @@ package se.lexicon.dao.impl;
 import org.springframework.stereotype.Component;
 import se.lexicon.dao.CustomerDao;
 import se.lexicon.dao.sequencer.AccountIdGenerator;
+import se.lexicon.exception.DataNotFoundException;
 import se.lexicon.model.Customer;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class CustomerDaoImpl implements CustomerDao {
@@ -37,7 +39,28 @@ public class CustomerDaoImpl implements CustomerDao {
     }
 
     @Override
-    public void remove(Long aLong) {
+    public void remove(Long customerId) throws DataNotFoundException {
+       Optional<Customer> optionalCustomer = findById(customerId);
+       if(!optionalCustomer.isPresent()) throw new DataNotFoundException("data not found exception");
+        else storage.remove(optionalCustomer.get());
+
+    }
+
+    @Override
+    public List<Customer> finBuName(String firstName) {
+        List<Customer> filteredList = new ArrayList<>();
+        /*if(firstName==null)throw new IllegalArgumentException("FirstName was null");
+        for (Customer element : storage){
+            if (element.getFirstName().equals(firstName)){
+                filteredList.add(element);
+
+            }
+        }
+        return filteredList;*/           // for and each statement
+
+        return storage.stream()
+                .filter(element-> element.getFirstName().equals(firstName))
+                .collect(Collectors.toList());
 
     }
 }
